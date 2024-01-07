3a0edd0948e97141c236a2cdc1384b3876c7a6c3
Towaris4 <ivan.tarasov2488@gmail.com>
2024-01-07 19:22:52 +0300

    1.7. Конструктор [#173889]

---
+++ src/main/java/ru/job4j/oop/Error.java
@@ -0,0 +1,31 @@
+package ru.job4j.oop;
+
+public class Error {
+    private boolean active;
+    private int status;
+    private String message;
+
+    public Error() {
+    }
+
+    public Error(boolean active, int status, String message) {
+        this.active = active;
+        this.status = status;
+        this.message = message;
+    }
+
+    public void errorInfo() {
+            System.out.println("Active: " + this.active);
+            System.out.println("Status: " + this.status);
+            System.out.println("Message: " + this.message);
+    }
+
+    public static void main(String[] args) {
+        Error error1 = new Error();
+        Error error2 = new Error(true, 1, "guy");
+        Error error3 = new Error(false, 2, "superman");
+        error1.errorInfo();
+        error2.errorInfo();
+        error3.errorInfo();
+    }
+}
