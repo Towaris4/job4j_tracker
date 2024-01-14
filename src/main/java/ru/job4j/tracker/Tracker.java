bff4fd2878eb649066c75193df6ca8e2e41e9fa6
Towaris4 <ivan.tarasov2488@gmail.com>
2024-01-14 19:55:53 +0300

    8. Что такое валидация? [#246864]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -47,18 +47,21 @@ public class Tracker {
 
     public boolean replace(int id, Item item) {
         int index = indexOf(id);
-        if (index == -1) {
-            return false;
+        boolean result = index != -1;
+        if (result) {
+            items[index] = item;
+            item.setId(id);
         }
-        items[index] = item;
-        item.setId(id);
-        return true;
+        return result;
     }
 
     public void delete(int id) {
         int index = indexOf(id);
-        System.arraycopy(items, index + 1, items, index, size - index - 1);
-        items[size - 1] = null;
-        size--;
+        boolean result = index != -1;
+        if (result) {
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
+            items[size - 1] = null;
+            size--;
+        }
     }
 }
