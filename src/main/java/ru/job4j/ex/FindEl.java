9517c384f9911694d11cc3fc3cb45bfa808291a1
Towaris4 <ivan.tarasov2488@gmail.com>
2024-01-21 18:18:00 +0300

    0.4. Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,25 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int result = -1;
+        for (int index = 0; index < value.length; index++) {
+            if (value[index].equals(key)) {
+                result = index;
+            }
+        }
+        if (result == -1) {
+            throw new ElementNotFoundException("Element don't found");
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String[] value = {"kapsha", "captcha", "kyshak"};
+        try {
+            indexOf(value, "kapsha");
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
