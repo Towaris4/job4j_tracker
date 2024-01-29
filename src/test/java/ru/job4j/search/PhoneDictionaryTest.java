da75c84c603794f3eda9d81506cb8e0679d5f922
Towaris4 <ivan.tarasov2488@gmail.com>
2024-01-29 22:16:11 +0300

    4. Телефонный справочник на базе ArrayList [#41598 #477295]

--- src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -16,4 +16,14 @@ public class PhoneDictionaryTest {
         ArrayList<Person> persons = phones.find("Petr");
         assertThat(persons.get(0).getSurname()).isEqualTo("Arsentev");
     }
+
+    @Test
+    public void whenNotFind() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("1234");
+        assertThat(persons.isEmpty()).isEqualTo(true);
+    }
 }
