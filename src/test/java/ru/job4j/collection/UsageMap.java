4b1908399d8dfc84d2291bc4b9642f953915a320
Towaris4 <ivan.tarasov2488@gmail.com>
2024-02-02 21:25:54 +0300

    0. Map, HashMap [#212652]

---
+++ src/test/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,17 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String, String> map = new HashMap<>();
+        map.put("Trsv1vn@gmail.com", "Tarasov Ivan Vladimirovich");
+        map.put("perfectworld@gmail.com", "Petrov Abdula Andeevich");
+        for (Map.Entry<String, String> entry : map.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            System.out.println(key + " = " + value);
+        }
+    }
+}
