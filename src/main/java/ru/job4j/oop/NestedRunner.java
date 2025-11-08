5082729ef5e5bd91c09f3084775c6cda83739f47
Towaris4 <ivan.tarasov2488@gmail.com>
2025-11-08 22:31:59 +0300

    1. Профилирование. [#1591]

---
+++ src/main/java/ru/job4j/oop/NestedRunner.java
@@ -0,0 +1,25 @@
+package ru.job4j.oop;
+
+public class NestedRunner {
+    private static int numberOne;
+    private int numberTwo;
+
+    public NestedRunner() {
+    }
+
+    static class StaticNested {
+        void setStaticOuterVariable(int value) {
+            numberOne = value;
+        }
+    }
+
+    public static void main(String[] args) {
+        numberOne = 1;
+        StaticNested nested = new StaticNested();
+        nested.setStaticOuterVariable(2);
+        System.out.println("num1 = " + numberOne);
+        StaticNested nested2 = new StaticNested();
+        nested2.setStaticOuterVariable(2);
+        System.out.println("num1 = " + numberOne);
+    }
+}
